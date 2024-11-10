2bbd019cc9c3da5f2e9f657f847f550e7b109ebe
bishvajit <112610641+bishvajit@users.noreply.github.com>
2024-11-10 11:43:44 +0530

    J.S. Commit

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Set current year in footer
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// Smooth scrolling for nav links
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "Auto",
+      behavior: "smooth"
     });
   });
 });
