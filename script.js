0cdc59ac42bf57c294bf280b129b89071eb2b4a7
bishvajit <112610641+bishvajit@users.noreply.github.com>
2024-11-16 11:49:53 +0530

    Script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
+      behavior: "quick"
     });
   });
 });
