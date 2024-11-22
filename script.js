c3764ff6c7b4cfbab7c232125a6d09fb6c36a0ca
bishvajit <112610641+bishvajit@users.noreply.github.com>
2024-11-22 11:56:19 +0530

    Script.j.s.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 
+
 document.querySelectorAll("nav a").forEach(link => {
   link.addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "quick"
+      behavior: "smooth"
     });
   });
 });
+
+
+const sections = document.querySelectorAll('.animate');
+
+function checkVisibility() {
+  const triggerBottom = window.innerHeight * 0.85; // 85% viewport height
+  sections.forEach(sec => {
+    const boxTop = sec.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      sec.classList.add('visible');
+    }
+  });
+}
+
+
+window.addEventListener('scroll', checkVisibility);
+window.addEventListener('load', checkVisibility);
